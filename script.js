079550cb17b44f8c0065c9fe488f64b1e37a5e75
LitriyHis <medvedka854@gmail.com>
2023-12-26 13:53:50 +0700

    addaptive

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 const body = document.body;
 const slider = document.getElementById("slider")
+const burger = document.getElementById("burger")
+const sidebar = document.getElementById("sidebar")
+const page = document.getElementById("page")
 
 function themeSwitch(){
   body.classList.toggle("dark");
 }
 
+function menuOpen(){
+  sidebar.style.display = "block";
+  sidebar.style.width = "240px";
+  burger.style.display = "none";
+  page.style.display = "block";
+  sidebar.style.position = "absolute"
+}
+
+function menuLock(){
+  sidebar.removeAttribute("style");
+  burger.removeAttribute("style");
+  page.removeAttribute("style")
+}
+
 slider.addEventListener("click", themeSwitch);
+
+burger.addEventListener("click", menuOpen);
+page.addEventListener("click", menuLock);
+
